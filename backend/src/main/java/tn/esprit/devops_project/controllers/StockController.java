8717a7f9b938e78aadd1c68580f7768923046b8d
Ahmed Abid <92954851+AhmedAb1d@users.noreply.github.com>
2023-11-08 00:26:14 +0100

    Update StockController.java

--- backend/src/main/java/tn/esprit/devops_project/controllers/StockController.java
+++ backend/src/main/java/tn/esprit/devops_project/controllers/StockController.java
@@ -19,6 +19,7 @@ public class StockController {
         return stockService.addStock(stock);
     }
 
+
     @GetMapping("/stock/{id}")
     Stock retrieveStock(@PathVariable Long id){
         return stockService.retrieveStock(id);
